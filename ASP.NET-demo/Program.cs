33eafc7cb6d622ab52796a834853de807a89c256
Vuong Ngo <vuong@mieli.de>
2023-06-23 14:15:29 +0200

    enable swagger on prod, minor changes on ports

--- ASP.NET-demo/Program.cs
+++ ASP.NET-demo/Program.cs
@@ -26,11 +26,8 @@ builder.Services.AddDbContext<ApiContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 using (var serviceScope = app.Services.CreateScope())
 {
